53df7d4fe7cb1d477b74e998b97064ff667665bd
dinimutmainahgafur-coder <dinimutmainahgafur@gmail.com>
2025-12-25 06:15:25 +0700

    ubah tekweb_prak9

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-
   basePath: '/tekweb_prak9',
   assetPrefix: '/tekweb_prak9/',
-
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
 };
-
 export default nextConfig;
