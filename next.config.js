ccbd2187ef882ffda76b126b0f05c96ec09160c3
dinimutmainahgafur-coder <dinimutmainahgafur@gmail.com>
2025-12-25 23:35:29 +0700

    Update next.config.js untuk GitHub

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: '/tekweb_prak9',   // tambahkan ini
+  assetPrefix: '/tekweb_prak9', // tambahkan ini supaya _next/static juga ikut prefix
 };
 
 module.exports = nextConfig;
